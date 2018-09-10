9ed658c62238115763539cd6587341eb9cb6db22
Тинитилов Дмитрий Анатольевич <Tinitilov@od.itstep.local>
2018-09-10 15:21:52 +0300

    adding public directory

--- routes.js
+++ routes.js
@@ -16,7 +16,26 @@ module.exports = function(app, db) {
 				var products = await catalog.find({}).toArray();
 
 				//res.render('start',{products:products});
-				res.end('[{"name":"iPhone"},{"name":"Samsung"},{"name":"Huawei"}]');
+				//res.end('[{"name":"iPhone"},{"name":"Samsung"},{"name":"Huawei"}]');
+			} catch(e) {
+				console.log(e);
+			}
+		})()
+	});
+
+	app.get('/add_product', function(req,res){
+		(async function() {
+
+			try {
+				var catalog = db.collection("catalog");
+
+				//req.body.name
+
+				await catalog.insert({"name":req.query.name})
+
+				res.redirect('/');
+				res.end();
+
 			} catch(e) {
 				console.log(e);
 			}
